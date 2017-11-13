8bc07aac944a28386a60a72fb92499272874f4da
panwaraporn <58030219@kmitl.ac.th>
2017-11-14 02:52:22 +0700

    commit Lab08P10-2

--- GDIPlus_1/GDIPlus_1/Form1.cs
+++ GDIPlus_1/GDIPlus_1/Form1.cs
@@ -21,15 +21,43 @@ namespace GDIPlus_1
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            Pen pen = new Pen(Color.Green, 2);
-            pen.DashStyle = DashStyle.DashDot;
-          //  Point point1 = new Point(100, 100);
-         //   Point point2 = new Point(200, 200);
-            e.Graphics.DrawLine(pen, 10,80,220,80);
-            pen.Dispose();
-            pen = new Pen(Brushes.DeepSkyBlue, 4);
-            e.Graphics.DrawLine(pen, 10, 120, 220, 120);
-            pen.Dispose();
+            Pen[] objPen = new Pen[11];
+            for (int i = 0; i < 11; i++) ;
+            {
+                objPen[i] = new Pen(Color.Blue, 9);
+            }
+
+            objPen[0].EndCap = LineCap.AnchorMask;
+            objPen[1].EndCap = LineCap.ArrowAnchor;
+            objPen[2].EndCap = LineCap.Custom;
+            objPen[3].EndCap = LineCap.DiamondAnchor;
+            objPen[4].EndCap = LineCap.Flat;
+            objPen[5].EndCap = LineCap.NoAnchor;
+            objPen[6].EndCap = LineCap.Round;
+            objPen[7].EndCap = LineCap.RoundAnchor;
+            objPen[8].EndCap = LineCap.Square;
+            objPen[9].EndCap = LineCap.SquareAnchor;
+            objPen[10].EndCap = LineCap.Triangle;
+
+            for (int i = 0; i < 11; i++) ;
+            {
+                g.DrawLine(objPen[i], 10, 10 + 20 * i, 200, 10 + 20 * i);
+                objPen[i].Dispose();
+            }
+            g.Dispose();
+        }
+
+
+
+           // Pen pen = new Pen(Color.Green, 2);
+           //  pen.DashStyle = DashStyle.DashDot;
+           //  Point point1 = new Point(100, 100);
+           //   Point point2 = new Point(200, 200);
+           //   e.Graphics.DrawLine(pen, 10,80,220,80);
+           //   pen.Dispose();
+           //  pen = new Pen(Brushes.DeepSkyBlue, 4);
+           //  e.Graphics.DrawLine(pen, 10, 120, 220, 120);
+           //   pen.Dispose();
 
             //   HatchBrush mybrush = new HatchBrush(HatchStyle.DarkVertical, Color.White, Color.Violet);
             //  Pen mypen = new Pen(mybrush,5);
