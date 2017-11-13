ca3badd88ca03b9f210d6d15bef6f19b20bb36d3
panwaraporn <58030219@kmitl.ac.th>
2017-11-14 00:24:49 +0700

    commit Lab08P07-2

--- GDIPlus_1/GDIPlus_1/Form1.cs
+++ GDIPlus_1/GDIPlus_1/Form1.cs
@@ -21,8 +21,9 @@ namespace GDIPlus_1
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            Brush mybrush = new SolidBrush(Color.Black, 1);
-            Pen mypen = new Pen(mybrush,5);
+            HatchBrush mybrush = new HatchBrush(HatchStyle.DarkVertical
+           Color.White,Color.Violet);
+           Pen mypen = new Pen(mybrush,5);
             g.DrawRectangle(mypen, 10, 10, 200, 200);
            // mypen.Width = 4;
            // mypen.Color = Color.Pink;
