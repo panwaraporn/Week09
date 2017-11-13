a365d357589feb47f828847043c8c1d724255f1d
panwaraporn <58030219@kmitl.ac.th>
2017-11-14 02:39:07 +0700

    commit Lab08P08-2

--- GDIPlus_1/GDIPlus_1/Form1.cs
+++ GDIPlus_1/GDIPlus_1/Form1.cs
@@ -21,22 +21,27 @@ namespace GDIPlus_1
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            HatchBrush mybrush = new HatchBrush(HatchStyle.DarkVertical
-           Color.White,Color.Violet);
-           Pen mypen = new Pen(mybrush,5);
-            g.DrawRectangle(mypen, 10, 10, 200, 200);
-           // mypen.Width = 4;
-           // mypen.Color = Color.Pink;
-           // g.DrawEllipse(mypen, 10, 10, 200, 200);
-            mypen.Dispose();
-            mybrush.Dispose();
+            Pen blackPen = new Pen(Color.Black, 3);
+            Point point1 = new Point(100, 100);
+            Point point2 = new Point(200, 200);
+            e.Graphics.DrawLine(blackPen, point1, point2);
+            blackPen.Dispose();
 
-     //       Pen somepen = new Pen(Color.FromArgb(255, 120, 200));
-       //     g.DrawEllipse(somepen, 20, 20, 200, 200);
-       //     somepen.Dispose();
+            //   HatchBrush mybrush = new HatchBrush(HatchStyle.DarkVertical, Color.White, Color.Violet);
+            //  Pen mypen = new Pen(mybrush,5);
+            //   g.DrawRectangle(mypen, 10, 10, 200, 200);
+            // mypen.Width = 4;
+            // mypen.Color = Color.Pink;
+            // g.DrawEllipse(mypen, 10, 10, 200, 200);
+            //  mypen.Dispose();
+            //  mybrush.Dispose();
 
-       //     Color col = Color.FromName("LightGreen");
-       //     this.BackColor = col;
+            //       Pen somepen = new Pen(Color.FromArgb(255, 120, 200));
+            //     g.DrawEllipse(somepen, 20, 20, 200, 200);
+            //     somepen.Dispose();
+
+            //     Color col = Color.FromName("LightGreen");
+            //     this.BackColor = col;
 
         }
     }
