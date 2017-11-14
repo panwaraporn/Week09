49093b6fd92b9725d21cf0ca2b3c0cb0185c6b21
panwaraporn <58030219@kmitl.ac.th>
2017-11-14 22:43:24 +0700

    commit Lab08P16-2

--- GDIPlus_1/GDIPlus_1/Form1.cs
+++ GDIPlus_1/GDIPlus_1/Form1.cs
@@ -22,17 +22,13 @@ namespace GDIPlus_1
         {
             Graphics g = e.Graphics;
 
-            Rectangle rect = new Rectangle(10, 10, 100, 100);
-            g.FillEllipse(Brushes.Purple, rect);
+            Pen penEllipse = new Pen(Color.Brown);
+            penEllipse.DashStyle = DashStyle.Dash;
+            g.DrawEllipse(penEllipse, 20, 20, 200, 150);
 
-            Rectangle rect1 = new Rectangle(10, 120, 100, 100);
-            g.FillEllipse(Brushes.DeepPink, rect1);
+            Pen penArc = new Pen(Color.Magenta, 2);
+            g.DrawArc(penArc, 20, 20, 200, 150, 90, 180);
 
-            Rectangle rect2 = new Rectangle(150, 10, 120, 100);
-            g.FillEllipse(Brushes.DarkSlateBlue, rect2);
-
-            Rectangle rect3 = new Rectangle(10, 10, 100, 100);
-            g.FillEllipse(Brushes.Firebrick, rect3);
 
             g.Dispose();
         }
